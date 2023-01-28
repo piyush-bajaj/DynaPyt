ba8605cde530a06fad07eb93f466a6a58347bf6a
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-28 10:32:09 +0100

    printing the number of times the concerned loop is called

--- src/dynapyt/analyses/LoopingAnalysis.py
+++ src/dynapyt/analyses/LoopingAnalysis.py
@@ -10,27 +10,32 @@ class LoopingAnalysis(BaseAnalysis):
     def __init__(self) -> None:
         super().__init__()
         logging.basicConfig(filename='output.log', format='%(message)s', level=logging.INFO)
-        self.for_loop_count = 0
-        self.while_loop_count = 0
-        
+        self.for_loop_count = dict()
+        self.while_loop_count = dict()
+
     def begin_execution(self) -> None:
-        logging.info('Starting Looping Analysis')
+        logging.info('\tStarting Looping Analysis\t')
         
     def end_execution(self) -> None:
-        logging.info('Number of for loops : {}'.format(self.for_loop_count))
-        logging.info('Number of while loops : {}'.format(self.while_loop_count))
+        for key, value in self.for_loop_count:
+            logging.info('Executed condition \t{}\t in file \t{}\t \t{}\t times'.format(key[2], key[1], value))
+        for key, value in self.while_loop_count:
+            logging.info('Executed condition \t{}\t in file \t{}\t \t{}\t times with condition value as {}'.format(key[2], key[1], value, key[3]))
         
     def enter_for(self, dyn_ast: str, iid: int, next_value: Any) -> Optional[Any]:
-        self.for_loop_count += 1
         ast, iids = self._get_ast(dyn_ast)
-        node = get_node_by_location(ast, iids.iid_to_location[iid], m.For())
-        loop_string = cst.parse_module('').code_for_node(node).split(':')[0]
-        # loop_string = 'for {} in {}'.format(target, iter)
-        logging.info('Entering for loop : \n{}'.format(loop_string))
+        node = get_node_by_location(ast, iids.iid_to_location[iid], m.For())        
+        loc_file = self.iid_to_location(dyn_ast, iid)
+        # loop_string = cst.parse_module('').code_for_node(node).split(':')[0]
+        loop_string = 'for {} in {}'.format(node.target, node.iter)
+        self.for_loop_count[(iid, loc_file[0], loop_string)] = self.for_loop_count.get((iid, loc_file[0], loop_string), 0) + 1
+        logging.debug('Entering for loop : \n{}'.format(loop_string))
 
     def enter_while(self, dyn_ast: str, iid: int, cond_value: bool) -> Optional[bool]:
-        self.while_loop_count += 1
         ast, iids = self._get_ast(dyn_ast)
         node = get_node_by_location(ast, iids.iid_to_location[iid], m.While())
-        condition = cst.parse_module('').code_for_node(node)
-        logging.info('Entering while loop : \n{}'.format(condition))
+        loc_file = self.iid_to_location(dyn_ast, iid)
+        #loop_string = cst.parse_module('').code_for_node(node).split(':')[0]
+        loop_string = 'while {}'.format(node.test)
+        self.while_loop_count[(iid, loc_file[0], loop_string, cond_value)] = self.for_loop_count.get((iid, loc_file[0], loop_string, cond_value), 0) + 1
+        logging.debug('Entering while loop : \n{}'.format(loop_string))
