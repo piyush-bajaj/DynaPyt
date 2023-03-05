8c9578792e720fabf1fa8a130a8e3d4accfafcb3
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-03-05 07:49:15 +0100

    logger

--- src/dynapyt/analyses/CallGraph.py
+++ src/dynapyt/analyses/CallGraph.py
@@ -1,4 +1,5 @@
 from typing import Callable, Tuple, Dict
+import logging
 import libcst.matchers as m
 from .BaseAnalysis import BaseAnalysis
 from ..utils.nodeLocator import get_node_by_location, get_parent_by_type
@@ -6,13 +7,13 @@ from ..utils.nodeLocator import get_node_by_location, get_parent_by_type
 class CallGraph(BaseAnalysis):
     def __init__(self):
         super(CallGraph, self).__init__()
+        logging.basicConfig(format='%(message)s', level=logging.INFO)
         self.graph = set()
 
     def pre_call(self, dyn_ast: str, iid: int, pos_args: Tuple, kw_args: Dict, function: Callable):
         ast, iids = self._get_ast(dyn_ast)
         caller = get_parent_by_type(ast, iids.iid_to_location[iid], m.FunctionDef())
         callee = get_node_by_location(ast, iids.iid_to_location[iid], m.Call())
-        print("graph", self.graph)
         if caller is None:
             f = 'root module'
         else:
@@ -22,6 +23,9 @@ class CallGraph(BaseAnalysis):
         else:
             t = callee.func.value
         self.graph.add((f, t))
+        logging.info('Added to graph')
+        logging.info('Caller : {}\nTarget : {}'.format(f,t))
+        logging.info('New Graph : {}'.format(self.graph))
     
     def end_execution(self):
         print(self.graph)
