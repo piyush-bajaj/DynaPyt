68497f30673636920cbdfe4fbf26644f0d7de795
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-04-23 22:34:27 +0200

    Commenting module part

--- src/dynapyt/analyses/CallGraph.py
+++ src/dynapyt/analyses/CallGraph.py
@@ -18,13 +18,14 @@ class CallGraph(BaseAnalysis):
     '''
     def pre_call(self, dyn_ast: str, iid: int, function: Callable, pos_args: Tuple, kw_args: Dict):
         ast, iids = self._get_ast(dyn_ast)
-        module = getmodule(function)
-        module = str(module).split(' ')[1] if module is not None else "''"
+        # module = getmodule(function)
+        # module = str(module).split(' ')[1] if module is not None else "''"
         # calling function 
         caller = get_parent_by_type(ast, iids.iid_to_location[iid], m.FunctionDef())
         # called function
         if hasattr(function, "__qualname__"):
-            callee = module[1:-1] + '.' + function.__qualname__ if module != "''" else function.__qualname__
+            # callee = module[1:-1] + '.' + function.__qualname__ if module != "''" else function.__qualname__
+            callee = function.__qualname__
         else:
             temp = str(function)
             callee = temp
